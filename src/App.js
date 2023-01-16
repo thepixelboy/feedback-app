6fc41fbd9bd9f007c4cc696c559c009cb63ad36d
Ruben <34570952+thepixelboy@users.noreply.github.com>
2023-01-16 16:20:51 +0100

    feat: conditionals in jsx

--- src/App.js
+++ src/App.js
@@ -7,19 +7,28 @@ function App() {
     { id: 3, text: "Comment three" },
   ];
 
+  const loading = false;
+  const showComments = true;
+
+  if (loading) return <h1>Loading...</h1>;
+
+  const commentBlock = (
+    <div className="comments">
+      <h3>Comments ({comments.length})</h3>
+      <ul>
+        {comments.map((comment, index) => (
+          <li key={index}>{comment.text}</li>
+        ))}
+      </ul>
+    </div>
+  );
+
   return (
     <div className="container">
       <h1>{title}</h1>
       <p>{body}</p>
 
-      <div className="comments">
-        <h3>Comments ({comments.length})</h3>
-        <ul>
-          {comments.map((comment, index) => (
-            <li key={index}>{comment.text}</li>
-          ))}
-        </ul>
-      </div>
+      {showComments && commentBlock}
     </div>
   );
 }
