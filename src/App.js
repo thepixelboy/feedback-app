744c8b215d1865379a93fee294f59f35e646bb33
Ruben <34570952+thepixelboy@users.noreply.github.com>
2023-01-16 16:31:29 +0100

    feat: create your first component and props

--- src/App.js
+++ src/App.js
@@ -1,35 +1,13 @@
+import Header from "./components/Header";
+
 function App() {
-  const title = "Welcome to the new blog";
-  const body = "This is the body of the blog";
-  const comments = [
-    { id: 1, text: "Comment one" },
-    { id: 2, text: "Comment two" },
-    { id: 3, text: "Comment three" },
-  ];
-
-  const loading = false;
-  const showComments = true;
-
-  if (loading) return <h1>Loading...</h1>;
-
-  const commentBlock = (
-    <div className="comments">
-      <h3>Comments ({comments.length})</h3>
-      <ul>
-        {comments.map((comment, index) => (
-          <li key={index}>{comment.text}</li>
-        ))}
-      </ul>
-    </div>
-  );
-
   return (
-    <div className="container">
-      <h1>{title}</h1>
-      <p>{body}</p>
-
-      {showComments && commentBlock}
-    </div>
+    <>
+      <Header />
+      <div className="container">
+        <h1>My App</h1>
+      </div>
+    </>
   );
 }
 
