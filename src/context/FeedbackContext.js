011afa8d28d5a156fe6f7a65093e837fe0cc9fa7
Ruben <34570952+thepixelboy@users.noreply.github.com>
2023-01-20 12:02:37 +0100

    feat: update feedback item

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -39,6 +39,13 @@ export const FeedbackProvider = ({ children }) => {
     }
   };
 
+  // Update feedback item
+  const updateFeedback = (id, updItem) => {
+    setFeedback(
+      feedback.map((item) => (item.id === id ? { ...item, ...updItem } : item))
+    );
+  };
+
   // Se item to be updated
   const editFeedback = (item) => {
     setFeedbackEdit({
@@ -51,10 +58,11 @@ export const FeedbackProvider = ({ children }) => {
     <FeedbackContext.Provider
       value={{
         feedback,
+        feedbackEdit,
         addFeedback,
         deleteFeedback,
         editFeedback,
-        feedbackEdit,
+        updateFeedback,
       }}
     >
       {children}
